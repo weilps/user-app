d249aa4c05b39f6f5fd6b6230c49fe2d8abfad03
weilps <guillaume.gaudin75@gmail.com>
2023-03-21 11:08:18 +1100

    v1.0
    functional

--- index.js
+++ index.js
@@ -12,12 +12,30 @@ const fetchUser = async () => {
 const userDisplay = async () => {
   await fetchUser();
 
+  const dateParser = (date) => {
+    let newDate = new Date(date).toLocaleDateString("en-AU", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    return newDate;
+  };
+
+  const dayCalc = (date) => {
+    let today = new Date();
+    let todayTimeStamp = Date.parse(today);
+    let timeStamp = Date.parse(date);
+    return Math.ceil((todayTimeStamp - timeStamp) / 8.64e7);
+  };
+
   document.body.innerHTML = userData
     .map(
       (user) => `
         <div class="card">
-            <img src=${user.picture.large} alt="photo de ${user.name.last}">
-            <h3>${user.name.first}</h3>
+            <img src=${user.picture.large} alt="photo of ${user.name.last}">
+            <h3>${user.name.first} ${user.name.last}</h3>
+            <p>${user.location.city}, ${dateParser(user.dob.date)}</p>
+            <em>Member since : ${dayCalc(user.registered.date)} days</em>
         </div>
   `
     )
